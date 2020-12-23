5c156e751e517814ae3cfc5f3d32919bdab974eb
Jose <Artixba@hotmail.com>
2020-12-23 14:50:09 -0600

    Added partial Newegg functionality. Doesn't add all prices

--- amazonScrape.py
+++ amazonScrape.py
@@ -10,7 +10,7 @@ page = requests.get(URL, headers=headers)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-title = soup.find("div", class_="sku-title").get_text(strip=True)
+title = soup.find("div", class_="sku-title").get_text()
 price = soup.find(
     "div", class_="priceView-hero-price priceView-customer-price").get_text(strip=True)
 
@@ -22,11 +22,25 @@ print(title)
 print(converted_price)
 print(URL)
 
-URLN = 'https://www.newegg.com/p/pl?d=rtx+3080&N=100007709%208000%20601357282&isdeptsrh=1'
+URLN = 'https://www.newegg.com/p/pl?d=rtx&N=100007709%20601357282&isdeptsrh'
 pagen = requests.get(URLN, headers=headers)
 soupn = BeautifulSoup(pagen.content, "html.parser")
 
-cards = soup.find_all()
+cards = soupn.find_all('div', {'class':'item-cell'})
 
-for c in cards:
-    print(c.get_text(strip=True))
+for card in cards:
+    card_title = card.find('a',{'class':'item-title'})
+    card_price = card.find('li', {'class':'price-current'})
+    try:
+        if (card_price.text != ""):
+            print(card_title.text)
+            print(" ")
+            print(card_price.text)
+            print(" ")
+            print(card_title['href'])
+
+    except:
+        pass
+    
+    # print(card_price)
+# print(cards)
